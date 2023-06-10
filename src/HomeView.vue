d4a951269046fb3fe70dd725ad939ef872f6415c
Dennis <dennisconcepcionmartin@gmail.com>
2023-06-10 11:11:13 +0100

    add project code links

--- src/HomeView.vue
+++ src/HomeView.vue
@@ -10,10 +10,15 @@ export default defineComponent({
 
 <template>
    <div class="container my-5">
-      <p>hey, it's the finance parser. your crypto bro friend who is always talking about how easy is to get rich.</p>
-      <p>this project was developed by me (Dennis). it's a serverless finance parser using a machine learning service
+      <p>hey, it's the finance parser. your crypto bro friend who is always talking about how easy is to get rich.
+         this project was developed by me (Dennis). it's a serverless finance parser using a machine learning service
          by AWS called Textract. check out my <a href="https://youtube.com/@denniscmartin" target="_blank">youtube</a>
          channel, the development process is documented there.</p>
+      <p>
+         Project code: <br>
+         - Frontend: <a href="https://github.com/denniscmartin/finance-parser-frontend" target="_blank">Link</a><br>
+         - Backend: <a href="https://github.com/denniscmartin/finance-parser" target="_blank">Link</a>
+      </p>
 
       <CompaniesTable/>
    </div>
